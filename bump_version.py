e45d5798b97e83b7529fa416d0b804bd66edae58
mayank0202 <mayank.sharma@sourcefuse.com>
2023-06-21 16:18:33 +0530

    feat: Add user authentication feature

--- bump_version.py
+++ bump_version.py
@@ -1,46 +1,46 @@
-import sys
-import subprocess
-import re
+# import sys
+# import subprocess
+# import re
 
-def get_latest_commit_message():
-    command = ["git", "log", "-1", "--pretty=format:%s"]
-    result = subprocess.run(command, capture_output=True, text=True)
-    return result.stdout.strip()
+# def get_latest_commit_message():
+#     command = ["git", "log", "-1", "--pretty=format:%s"]
+#     result = subprocess.run(command, capture_output=True, text=True)
+#     return result.stdout.strip()
 
-def get_version_from_commit_message(commit_message):
-    pattern = r"#(\d+\.\d+\.\d+)"
-    matches = re.findall(pattern, commit_message)
-    if matches:
-        return matches[0]  # Return the first match
-    return None
+# def get_version_from_commit_message(commit_message):
+#     pattern = r"#(\d+\.\d+\.\d+)"
+#     matches = re.findall(pattern, commit_message)
+#     if matches:
+#         return matches[0]  # Return the first match
+#     return None
 
 
-def bump_version(previous_version, bump_type):
-    major, minor, patch = previous_version.split('.')
-    if bump_type == "major":
-        major = str(int(major) + 1)
-        minor = "0"
-        patch = "0"
-    elif bump_type == "minor":
-        minor = str(int(minor) + 1)
-        patch = "0"
-    elif bump_type == "patch":
-        patch = str(int(patch) + 1)
-    else:
-        print("Invalid bump_type provided")
-        sys.exit(1)
+# def bump_version(previous_version, bump_type):
+#     major, minor, patch = previous_version.split('.')
+#     if bump_type == "major":
+#         major = str(int(major) + 1)
+#         minor = "0"
+#         patch = "0"
+#     elif bump_type == "minor":
+#         minor = str(int(minor) + 1)
+#         patch = "0"
+#     elif bump_type == "patch":
+#         patch = str(int(patch) + 1)
+#     else:
+#         print("Invalid bump_type provided")
+#         sys.exit(1)
     
-    new_version = f"{major}.{minor}.{patch}"
-    return new_version
+#     new_version = f"{major}.{minor}.{patch}"
+#     return new_version
 
-if __name__ == "__main__":
-    bump_type = sys.argv[1]
-    latest_commit_message = get_latest_commit_message()
-    previous_version = get_version_from_commit_message(latest_commit_message)
+# if __name__ == "__main__":
+#     bump_type = sys.argv[1]
+#     latest_commit_message = get_latest_commit_message()
+#     previous_version = get_version_from_commit_message(latest_commit_message)
     
-    if previous_version is None:
-        print("No version tag found in the commit message.")
-        sys.exit(1)
+#     if previous_version is None:
+#         print("No version tag found in the commit message.")
+#         sys.exit(1)
     
-    new_version = bump_version(previous_version, bump_type)
-    print(new_version)
+#     new_version = bump_version(previous_version, bump_type)
+#     print(new_version)
